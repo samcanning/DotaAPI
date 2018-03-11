503f9f1419296b22f353c0a492e6a4f1a7e82d03
Sam Canning <samcanning@outlook.com>
2018-03-11 13:20:37 -0700

    hero creator works

--- Controllers/CreateController.cs
+++ Controllers/CreateController.cs
@@ -32,12 +32,57 @@ namespace DotaAPI.Controllers
             {
                 allSpells.Add(Convert(s));
             }
-            List<DisplaySpell> regulars = allSpells.Where(s => s.ultimate == false).ToList();
-            List<DisplaySpell> ultimates = allSpells.Where(s => s.ultimate == true).ToList();
-            List<List<DisplaySpell>> container = new List<List<DisplaySpell>>();
-            container.Add(regulars);
-            container.Add(ultimates);
-            return View(container);
+            ViewBag.heroes = _context.Heroes.ToList();
+            ViewBag.regulars = allSpells.Where(s => s.ultimate == false).ToList();
+            ViewBag.ultimates = allSpells.Where(s => s.ultimate == true).ToList();
+            return View();
+        }
+
+        [Route("create/submit")]
+        [HttpPost]
+        public IActionResult Submit(NewHeroValidator model)
+        {
+            List<DisplaySpell> allSpells = new List<DisplaySpell>();
+            foreach(Spell s in _context.Spells)
+            {
+                allSpells.Add(Convert(s));
+            }
+            ViewBag.heroes = _context.Heroes.ToList();
+            ViewBag.regulars = allSpells.Where(s => s.ultimate == false).ToList();
+            ViewBag.ultimates = allSpells.Where(s => s.ultimate == true).ToList();
+            if(model.hero_id == 0)
+            {
+                ModelState.AddModelError("hero_id", "You must select a base for your hero.");
+            }
+            if(model.spell_1 == 0 || model.spell_2 == 0 || model.spell_3 == 0)
+            {
+                ModelState.AddModelError("spell_1", "You must select three regular spells.");
+            }
+            else if(model.spell_1 == model.spell_2 || model.spell_1 == model.spell_3 || model.spell_2 == model.spell_3)
+            {
+                ModelState.AddModelError("spell_1", "Cannot use the same spell twice.");
+            }
+            if(model.spell_4 == 0)
+            {
+                ModelState.AddModelError("spell_4", "Your hero needs an ultimate.");
+            }
+            if(ModelState.IsValid)
+            {
+                New_Hero hero = new New_Hero(){
+                    name = model.name,
+                    hero_id = model.hero_id,
+                    spell_1_id = model.spell_1,
+                    spell_2_id = model.spell_2,
+                    spell_3_id = model.spell_3,
+                    spell_4_id = model.spell_4
+                };
+                if(model.bio.Length > 0) hero.bio = model.bio;
+                _context.Add(hero);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.bio = model.bio;
+            return View("Create");
         }
 
         public DisplaySpell Convert(Spell input)
@@ -82,6 +127,12 @@ namespace DotaAPI.Controllers
             spells.Add(Convert(_context.Spells.SingleOrDefault(s => s.id == newHero.spell_4_id)));
             display.spells = spells;
             return display;
+        }
+
+        [Route("test")]
+        public IActionResult Test()
+        {
+            return Json(ConvertHero(2));
         }
     }
 }
